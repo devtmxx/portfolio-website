46b559cf26cace326e409e0fb813eb1d9d051b63
Tim Mauersberger <tim-mauersberger@gmx.de>
2025-02-23 12:46:32 +0100

    Using local font now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,51 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Urbanist } from "next/font/google";
+import localFont from "next/font/local";
 import Header from "@/components/sections/header";
-import clsx from "clsx";
 import Footer from "@/components/footer";
+import React from "react";
 
-const urbanist = Urbanist({ subsets: ["latin"] });
+const urbanist = localFont({
+  src: [
+    {
+      path: "./fonts/Urbanist-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Urbanist-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-urbanist",
+});
 
 export const metadata: Metadata = {
   title: "devtmxx.de",
@@ -18,13 +58,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-900">
-      <body
-        className={clsx(
-          urbanist.className,
-          "relative min-h-screen antialiased",
-        )}
-      >
+    <html lang="en" className={`bg-slate-900 ${urbanist.variable}`}>
+      <body className="relative min-h-screen antialiased">
         <Header />
         {children}
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
