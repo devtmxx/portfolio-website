b6b7298e10f8b37673b2f2a8a261a528edfd5910
Tim Mauersberger <tim-mauersberger@gmx.de>
2025-03-29 17:01:47 +0100

    Added a disclaimer to my work page

--- app/(root)/my-work/page.tsx
+++ app/(root)/my-work/page.tsx
@@ -5,6 +5,11 @@ const MyWorkPage = () => {
   return (
     <Bounded>
       <ContentList />
+
+      <p className="text-slate-100 text-xl mt-8">
+        These items only depict a very small part of what I have done in my
+        past. Feel free to contact me for even more references :)
+      </p>
     </Bounded>
   );
 };
